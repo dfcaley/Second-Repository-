ff41caa56eb01c03afd33dbd145ec37404a2b815
dfcaley <molly220>
2016-05-10 12:22:48 +0100

    2nd commit

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var greeting = 'Hi ';
-var name = prompt ('What is your name?');
-var greeting_message = greeting + name;
-alert (greeting_message);
+// var generateMessage = function (salutation, visitor_name) {
+//     var salutation_message = salutation + visitor_name;
+//     return salutation_message;
+// };
+//
+//
+// var greet = function () {
+//     var greeting = 'Hi ';
+//     var name = prompt('What is your name?');
+//     var greeting_message = generateMessage (greeting, name);
+//     alert(greeting_message);
+// };
+//
+// greet();
+
+var greet = function (event_details) {
+    alert ('Greet function ran');
+    event_details.preventDefault();
+};
+
+var form = document.getElementById ("signup-form");
+form.addEventListener("submit", greet);
+
