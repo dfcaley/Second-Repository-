a8d607ab08345042a00fa3d7e404fa2842b6197d
dfcaley <molly220>
2016-05-10 12:35:49 +0100

    2nd commit

--- script.js
+++ script.js
@@ -13,10 +13,20 @@
 //
 // greet();
 
+// var greet = function (event_details) {
+//     alert ('Greet function ran');
+//     event_details.preventDefault();
+// };
+
 var greet = function (event_details) {
-    alert ('Greet function ran');
+    var greeting = 'Thanks ';
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var greeting_message = greeting + name + ' You have been signed up as ' + email;
+    alert(greeting_message);
     event_details.preventDefault();
 };
+
 
 var form = document.getElementById ("signup-form");
 form.addEventListener("submit", greet);
