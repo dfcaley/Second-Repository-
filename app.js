0c5a8363013937291b6b360fa1fa19c5cac21014
dfcaley <molly220>
2016-05-10 16:01:57 +0100

    2nd commit

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require("express");
 var app = express ();
+var bodyParser = require("body-parser");
 
-app.listen(8000);
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/hello', function(request, response) {
+    response.send("Hello!!");
+});
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.listen(8000, function() {
+    console.log("Server started");
+});
