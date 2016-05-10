f6558e3037f8d1aa899074c041011396db77016e
dfcaley <molly220>
2016-05-10 16:59:55 +0100

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express ();
 var bodyParser = require("body-parser");
+var csv = require("ya-csv");
+
 
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,6 +15,21 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 
+app.post('/signup', function(request, response) {
+    var identity = request.body.visitorName;
+    var address = request.body.visitorEmail;
+
+
+    var database = csv.createCsvFileWriter("customerDetails", {flags: "a"});
+    var data = [identity, address];
+    database.writeRecord(data);
+    
+    database.writeStream.end();
+    
+    response.send(identity);
+});
+
 app.listen(8000, function() {
     console.log("Server started");
 });
+
