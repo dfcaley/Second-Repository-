9d4fca25c5de922149c998419531f9913b731f8d
dfcaley <molly220>
2016-05-11 17:19:25 +0100

    2nd commit

--- Static/script.js
+++ Static/script.js
@@ -18,22 +18,22 @@
 //     event_details.preventDefault();
 // };
 
-// var greet = function (event_details) {
-//     var greeting = "Thank you, ";
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var greeting_message = greeting + name + ". " + email + " has now been signed up to recieve Central Perk updates and offers.";
-//
-//     var container = document.getElementById("signup-form-container");
-//     container.innerHTML = "<p>" + greeting_message + "</p>";
-//
-//     event_details.preventDefault();
-//
-//     var tagline = document.getElementById("tagline");
-//     var blue = "rgb(91, 184, 255)";
-//     tagline.style.color = blue;
+var greet = function (event_details) {
+    var greeting = "Thank you, ";
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var greeting_message = greeting + name + ". " + email + " has now been signed up to recieve Central Perk updates and offers.";
 
-// };
+    var container = document.getElementById("signup-form-container");
+    container.innerHTML = "<p>" + greeting_message + "</p>";
+
+    event_details.preventDefault();
+
+    var tagline = document.getElementById("tagline");
+    var blue = "rgb(91, 184, 255)";
+    tagline.style.color = blue;
+
+};
 
 //     var greeting = 'Thanks ';
 //     var name = document.getElementById("name").value;
@@ -45,8 +45,8 @@
 
 
 //
-// //
-// var form = document.getElementById ("signup-form");
-// form.addEventListener("submit", greet);
+//
+var form = document.getElementById ("signup-form");
+form.addEventListener("submit", greet);
 
 
--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-var express = require("express");
-var app = express ();
-var bodyParser = require("body-parser");
-var csv = require("ya-csv");
-
-
-app.use(express.static(__dirname + '/static'));
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/hello', function(request, response) {
-    response.send("Hello!!");
-});
-
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + "/index.html");
-});
-
-app.post('/signup', function(request, response) {
-    var identity = request.body.visitorName;
-    var address = request.body.visitorEmail;
-
-
-    var database = csv.createCsvFileWriter("customerDetails", {flags: "a"});
-    var data = [identity, address];
-    database.writeRecord(data);
-   
-    database.writeStream.end();
-
-    response.send(identity);
-});
-
-app.listen(8000, function() {
-    console.log("Server started");
-});
-
+// var express = require("express");
+// var app = express ();
+// var bodyParser = require("body-parser");
+// var csv = require("ya-csv");
+//
+//
+// app.use(express.static(__dirname + '/static'));
+// app.use(bodyParser.urlencoded({extended:true}));
+//
+// app.get('/hello', function(request, response) {
+//     response.send("Hello!!");
+// });
+//
+// app.get('/', function(request, response) {
+//     response.sendFile(__dirname + "/index.html");
+// });
+//
+// app.post('/signup', function(request, response) {
+//     var identity = request.body.visitorName;
+//     var address = request.body.visitorEmail;
+//
+//
+//     var database = csv.createCsvFileWriter("customerDetails", {flags: "a"});
+//     var data = [identity, address];
+//     database.writeRecord(data);
+//   
+//     database.writeStream.end();
+//
+//     response.send(identity);
+// });
+//
+// app.listen(8000, function() {
+//     console.log("Server started");
+// });
+//
